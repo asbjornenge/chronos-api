dffafeec0b9ffc2593ff30c7d6bac6e951bd4dc5
Asbjørn Enge <asbjorn@hanafjedle.net>
2018-12-17 14:45:40 +0100

    Some basic files

---
+++ schema/schema.sql
@@ -0,0 +1,27 @@
+CREATE TABLE IF NOT EXISTS tasks (
+  id            serial PRIMARY KEY,
+  name          varchar(100),
+  cron          varchar(100),
+  paused        boolean,
+  created       timestamp not null,
+  updated       timestamp not null
+);
+
+CREATE TABLE IF NOT EXISTS steps (
+  id            serial PRIMARY KEY,
+  task          integer REFERENCES tasks,
+  command       text,
+  timeout       integer,
+  created       timestamp not null,
+  updated       timestamp not null
+);
+
+CREATE TABLE IF NOT EXISTS execs (
+  id            serial PRIMARY KEY,
+  step          integer REFERENCES steps,
+  stdout        text,
+  stderr        text,
+  exitcode      integer,
+  time_start    timestamp not null,
+  time_end      timestamp not null
+);
