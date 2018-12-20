7d4aa34eecf2f4937987c4a207ca539d0b73a8f8
Asbjørn Enge <asbjorn@hanafjedle.net>
2018-12-20 09:43:46 +0100

    Smupdates

--- utils.js
+++ utils.js
@@ -4,7 +4,7 @@ module.exports.getClient = function() {
   return new Client({
     host:     process.env['DB_HOST'] || 'localhost',
     port:     process.env['DB_PORT'] || 5432,
-    user:     process.env['DB_USER'] || 'postgres',
+    user:     process.env['DB_USER'] || 'chronos',
     password: process.env['DB_PASS'] || 'chronos',
     database: process.env['DB_NAME'] || 'chronos'
   })
