a3b68bf7a98c63150d718ee2ac63a75d37391d23
Asbjørn Enge <asbjorn@hanafjedle.net>
2019-02-11 14:27:29 +0100

    Improved sorting for dashboard

--- dashboard.js
+++ dashboard.js
@@ -5,17 +5,17 @@ const crud = require('./crud')
 module.exports.get = async function(req, res) {
   let client = utils.getClient()
   await client.connect()
-  let tasks = await crud.get(client, 'tasks').then(raw => raw.rows)
+  let tasks = await client.query(`select * from tasks order by created`)
+                        .then(raw => raw.rows)
   let steps,execs = [] 
   for (task of tasks) {
-    task.steps = await client.query(`select * from steps where task=${task.id}`)
+    task.steps = await client.query(`select * from steps where task=${task.id} order by created`)
                         .then(raw => raw.rows)
     for (step of task.steps) {
       step.execs = await client.query(`select * from execs where step=${step.id} order by time_end DESC limit 1`)
                         .then(raw => raw.rows)
     }
   }
-
   send(res, 200, tasks)
   await client.end()
 }
