73d0c1f89563ae378bc88282c0897a1dd1fb9452
Pål Sola <pal@smartm.no>
2022-01-14 11:44:09 +0100

    removed unintentional import.

--- metrics.js
+++ metrics.js
@@ -1,7 +1,6 @@
 const prom = require('prom-client')
 const parser = require('cron-parser')
 const utils = require('./utils')
-const { client_encoding } = require('pg/lib/defaults')
 
 var exec_success = new prom.Gauge({
   name: 'chronos_exec_success',
